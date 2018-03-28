f0b28e8fc2789d97587c1f9d10c14d95df72bc1a
Serge Fedonin <fedonin@gmail.com>
2018-03-28 12:01:38 +0300

    Homework-7 (with *). Add users to project, appuser1 and appuser2

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,12 @@ provider "google" {
   region  = "${var.region}"
 }
 
+resource "google_compute_project_metadata" "default" {
+  metadata {
+    "ssh-keys" = "appuser1:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
+  }
+}
+
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"
